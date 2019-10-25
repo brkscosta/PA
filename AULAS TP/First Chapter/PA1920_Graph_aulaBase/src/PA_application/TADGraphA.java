96b1f9c50c8ba6b2e09ae96896777e703a5acf5d
brkscosta <joanacosta97@hotmail.com>
2019-10-25 21:24:46 +0100

    More updates...

--- AULAS TP/First Chapter/PA1920_Graph_aulaBase/src/PA_application/TADGraphA.java
+++ AULAS TP/First Chapter/PA1920_Graph_aulaBase/src/PA_application/TADGraphA.java
@@ -19,28 +19,35 @@ public class TADGraphA {
     public static void main(String[] args) throws Exception {
         
         Graph<Local, Ponte> grafo = new GraphLEdgeList<>();
-        
         Local quintaDoConde = new Local("Quinta do Conde");
+        Vertex<Local> v1 = grafo.insertVertex(quintaDoConde);
         Local almada = new Local("Almada");
+        Vertex<Local> v2 = grafo.insertVertex(almada);
         Local barreiro = new Local("Barreiro");
+        Vertex<Local> v3 = grafo.insertVertex(barreiro);
+        Local lisboa = new Local("Lisboa");
+        Vertex<Local> v4 = grafo.insertVertex(lisboa);
         
-        Ponte ptAlmadaQuinta = new Ponte("Almada - Quinta Do Conde", 14);
-        Ponte ptBarreiroQuinta = new Ponte("Barreiro - Quinta Do Conde", 9);
         
-        grafo.insertVertex(quintaDoConde);
-        grafo.insertVertex(almada);
-        grafo.insertVertex(barreiro);
+        Ponte ptAlmadaQuinta = new Ponte("Vasco da Gama", 14);
+        Ponte ptBarreiroQuinta = new Ponte("Bento Gonçalves", 9);
+        Ponte pt25Abril = new Ponte("25 de Abril", 10);
         
-        grafo.insertEdge(almada, quintaDoConde, ptAlmadaQuinta);
-        grafo.insertEdge(barreiro, quintaDoConde, ptBarreiroQuinta);
+        grafo.insertEdge(v1, v2, ptAlmadaQuinta);
+        grafo.insertEdge(v1, v4, ptBarreiroQuinta);
+        grafo.insertEdge(v2, v3, pt25Abril);
         
         System.out.println(grafo.numEdges());
         System.out.println(grafo.numVertices());
         
-        System.out.println(grafo.vertices());
-        System.out.println(grafo.edges());
+        System.out.println("Vertex: " + grafo.vertices());
+        System.out.println("Edges: " + grafo.edges());
         
-        System.out.println(grafo.areAdjacent(quintaDoConde, almada));
+        System.out.println("Incidentes de Almada " + grafo.incidentEdges(v2));
+        System.out.println("Incidentes de Barreiro " + grafo.incidentEdges(v3));
+        
+        System.out.println(grafo.areAdjacent(v1, v3));
+        
         
     }
 

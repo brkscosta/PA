34379e285c3bb41cebef31f6cdf89167daff3db2
brkscosta <joanacosta97@hotmail.com>
2019-12-05 01:32:22 +0000

    Graphic interface done, must be add the API graph on the place of the image.

--- PROJETO/WebCrawler/src/Views/Home.java
+++ PROJETO/WebCrawler/src/Views/Home.java
@@ -8,20 +8,23 @@ package Views;
 import Controller.HomeController;
 import Controller.IHomeOperations;
 import Model.WebCrawler;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.NodeOrientation;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.AccessibleRole;
-import javafx.scene.Cursor;
-import javafx.scene.Group;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
+import javafx.geometry.VPos;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
@@ -32,11 +35,15 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -103,45 +110,118 @@ public class Home extends VBox implements Observer, IHomeOperations {
         this.menuBar.getMenus().addAll(menuFile, menuEdit, menuHelp);
 
         // Criteria type on left pane
+        final ToggleGroup group = new ToggleGroup();
         this.lblCriteria = new Label("Modos Pesquisa");
+        this.lblCriteria.setFont(new Font("Verdana", 16));
+        this.lblCriteria.setPadding(new Insets(0, 0, 15, 0));
+        lblCriteria.setMaxWidth(Double.MAX_VALUE);
+        AnchorPane.setLeftAnchor(lblCriteria, 0.0);
+        AnchorPane.setRightAnchor(lblCriteria, 0.0);
+        lblCriteria.setAlignment(Pos.CENTER);
+
         this.txtFieldURL = new TextField();
+        this.txtFieldURL.setId("textFieldSearchURL");
+        this.txtFieldURL.setPrefSize(6, 15);
+
         this.btnStartCrawler = new Button("GO!");
         this.rdBtnBreadthFirst = new RadioButton("Largura");
+        this.rdBtnBreadthFirst.setToggleGroup(group);
+        this.rdBtnBreadthFirst.setSelected(true);
+
         this.lblNumPages = new Label("Número de páginas");
         this.txtFieldNumPages = new TextField();
+        txtFieldNumPages.setId("textFieldNumPages");
+        txtFieldNumPages.setMinSize(10, 10);
         this.rdBtnBreadthFirstDepth = new RadioButton("Profundidade");
+        this.rdBtnBreadthFirstDepth.setToggleGroup(group);
+        this.rdBtnBreadthFirstDepth.setSelected(false);
         this.rdBtnIterative = new RadioButton("Iterativo");
-        
+        this.rdBtnIterative.setToggleGroup(group);
+        this.rdBtnIterative.setSelected(false);
+
         this.anchorPaneLeft = new AnchorPane();
-        this.anchorPaneLeft.getChildren().addAll(lblCriteria, txtFieldURL, btnStartCrawler,
+        VBox items = new VBox(lblCriteria, txtFieldURL, btnStartCrawler,
                 rdBtnBreadthFirst, lblNumPages, txtFieldNumPages, rdBtnBreadthFirstDepth,
                 rdBtnIterative);
-        
+        items.setPadding(new Insets(5, 0, 0, 10));
+        AnchorPane.setTopAnchor(items, 15.0);
+        AnchorPane.setLeftAnchor(items, 10.0);
+        AnchorPane.setRightAnchor(items, 10.0);
+        this.anchorPaneLeft.getChildren().add(items);
 
         // Statistics on rigth pane
         this.lblStatistics = new Label("Estatísticas");
         this.anchorPaneRigth = new AnchorPane();
-        this.anchorPaneRigth.getChildren().add(lblStatistics);
+        VBox vboxChart = barChart();
+        AnchorPane.setTopAnchor(vboxChart, 10.0);
+        AnchorPane.setLeftAnchor(vboxChart, 10.0);
+        AnchorPane.setRightAnchor(vboxChart, 80.0);
+        AnchorPane.setBottomAnchor(vboxChart, 80.0);
+        anchorPaneRigth.getChildren().addAll(vboxChart);
 
         //Center pane graph will shows here
+        VBox boxScroll = new VBox();
         this.lblWebCrawler = new Label("Welcome to your WebCrawler Graph");
-        this.scrollPaneGraph = new ScrollPane();
-        this.scrollPaneGraph.setContent(lblWebCrawler);
+        this.lblWebCrawler.setFont(new Font("Verdana", 16));
+        this.lblWebCrawler.setPadding(new Insets(0, 0, 15, 0));
+        try {
+            //Creating an image 
+            Image image;
+            image = new Image(new FileInputStream("D:\\Engenharia\\Ano 3\\PA\\PROJETO\\WebCrawler\\src\\Resources\\images\\graph.png"));
+            //Setting the image view 
+            ImageView imageView = new ImageView(image);
 
+            //Setting the position of the image 
+            imageView.setX(50);
+            imageView.setY(25);
+
+            //setting the fit height and width of the image view 
+            imageView.setFitHeight(455);
+            imageView.setFitWidth(500);
+
+            //Setting the preserve ratio of the image view 
+            imageView.setPreserveRatio(true);
+            boxScroll.getChildren().add(lblWebCrawler);
+            boxScroll.getChildren().add(imageView);
+            this.scrollPaneGraph = new ScrollPane();
+            this.scrollPaneGraph.setContent(boxScroll);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Home.class.getName()).log(Level.SEVERE, null, ex);
+        }
         this.splitPane = new SplitPane();
+        this.splitPane.setDividerPositions(0.5f, 1.3f, 0.4f);
         this.splitPane.getItems().addAll(anchorPaneLeft, scrollPaneGraph, anchorPaneRigth);
-        
-        
-        //Config HBox Bootom
-        this.bottomHBox = new HBox(5, new Label("HBOx"), new Label("Another lbl"));
-        bottomHBox.setStyle("-fx-background-color: #FFFF;");
-        bottomHBox.setAlignment(Pos.CENTER_LEFT);
-        bottomHBox.setMinHeight(40);
-        bottomHBox.setMaxHeight(40);
-        /*Creates layout*/
-        this.setSpacing(10);
-        this.getChildren().addAll(menuBar, splitPane, bottomHBox);
 
+        //Config HBox Bootom
+        Pane panelBottom = new Pane();
+        panelBottom.setPadding(new Insets(0, 410, 0, 410));
+        Label labelErros = new Label("Erros Aqui");
+        Label lblAnotherThing = new Label("Outra Coisa");
+        this.bottomHBox = new HBox();
+        HBox.setHgrow(panelBottom, Priority.ALWAYS);
+        this.bottomHBox.getChildren().addAll(labelErros, panelBottom, lblAnotherThing);
+        bottomHBox.setStyle("-fx-background-color: #867B71;");
+
+        /*Creates layout*/
+        Home.setVgrow(splitPane, Priority.ALWAYS);
+        this.getChildren().addAll(menuBar, splitPane, bottomHBox);
+        getStylesheets().add(this.getClass().getResource("/Resources/css/styles.css").toExternalForm());
+    }
+
+    private VBox barChart() {
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Devices");
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Visits");
+        BarChart barChart = new BarChart(xAxis, yAxis);
+        XYChart.Series dataSeries1 = new XYChart.Series();
+        dataSeries1.setName("2014");
+        dataSeries1.getData().add(new XYChart.Data("Desktop", 567));
+        dataSeries1.getData().add(new XYChart.Data("Phone", 65));
+        dataSeries1.getData().add(new XYChart.Data("Tablet", 23));
+        barChart.getData().add(dataSeries1);
+        VBox vbox = new VBox(barChart);
+        return vbox;
     }
 
     @Override

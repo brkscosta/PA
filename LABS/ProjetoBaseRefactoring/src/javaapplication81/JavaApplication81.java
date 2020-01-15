f77b006b94f6178d4732cff9c95b22fc5c65e5db
brkscosta <joanacosta97@hotmail.com>
2020-01-15 00:21:08 +0000

    Update JavaApplication81.java

--- LABS/ProjetoBaseRefactoring/src/javaapplication81/JavaApplication81.java
+++ LABS/ProjetoBaseRefactoring/src/javaapplication81/JavaApplication81.java
@@ -15,6 +15,7 @@ public class JavaApplication81 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
         Agenda minhaAgenda = new Agenda("Escola");
         minhaAgenda.adicionaContacto("Ana", 23413);
